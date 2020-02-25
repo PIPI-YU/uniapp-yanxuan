c1cf0826f02d98a63555b8bec8d629f809c28024
ytt <824796614@qq.com>
2020-02-25 22:50:09 +0800

    0225_uniapp

---
+++ pages/Home/index.vue
@@ -0,0 +1,212 @@
+<template>
+	<view id="homeContainer">
+		<view class="homeHead">
+			<view class="headTop">
+				<image src="//yanxuan-static.nosdn.127.net/hxm/yanxuan-wap/p/20161201/style/img/icon-normal/indexLogo-a90bdaae6b.png" mode=""></image>
+				<view class="search" @click="goSearch">
+					<image src="../../static/images/Home/search.png" mode=""></image>
+					<view class="searchText">搜索商品, 共22421款好物</view>
+				</view>
+				<view class="loginButton" @click="goPersonal">登录</view>
+			</view>
+			<view class="headBottom">
+				<view class="hide" v-show="isHide">
+					<scroll-view class="scroll-view" scroll-x="true">
+							<view class="scroll-item">推荐</view>
+							<view class="scroll-item">居家生活</view>
+							<view class="scroll-item">服饰鞋包</view>
+							<view class="scroll-item">美食酒水</view>
+							<view class="scroll-item">个护清洁</view>
+							<view class="scroll-item">母婴亲子</view>
+							<view class="scroll-item">运动旅行</view>
+							<view class="scroll-item">数码家电</view>
+							<view class="scroll-item">全球特色</view>
+					</scroll-view>
+				</view>
+				<view class="show" v-show="!isHide">
+					<view class="showTop">全部频道</view>
+					<view class="showBottom">
+						<view class="show-item">推荐</view>
+						<view class="show-item">居家生活</view>
+						<view class="show-item">服饰鞋包</view>
+						<view class="show-item">美食酒水</view>
+						<view class="show-item">个护清洁</view>
+						<view class="show-item">母婴亲子</view>
+						<view class="show-item">运动旅行</view>
+						<view class="show-item">数码家电</view>
+						<view class="show-item">全球特色</view>
+					</view>
+					<view class="mask"></view>
+				</view>
+				<view class="arrow" @click="point(isHide)">
+					<image src="//yanxuan-static.nosdn.127.net/hxm/yanxuan-wap/p/20161201/style/img/icon-normal/arrow-down-3-9b31adfa37.png" mode=""></image>
+				</view>
+			</view>
+		</view>
+		<view class="swiper-container">
+			
+		</view>
+		<view class="homeService">
+			
+		</view>
+		<view class="homeContent">
+			
+		</view>
+	</view>
+</template>
+
+<script type="text/ecmascript-6">
+	export default {
+		data(){
+			return{
+				isHide:true
+			}
+		},
+		methods:{
+			point(isHide){
+				this.isHide = !isHide
+			},
+			goSearch(){
+				uni.navigateTo({
+					url:"../Search/index"
+				})
+			},
+			goPersonal(){
+				uni.switchTab({
+					url:"../Personal/index"
+				})
+			}
+		}
+	}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+	#homeContainer
+		width 750upx
+		height 1334upx
+		.homeHead
+			width 750upx
+			height 148upx
+			position fixed
+			left 0
+			top 0
+			.headTop
+				display flex
+				align-items center
+				width 750px
+				height 88upx
+				line-height 88px
+				padding 0 30upx
+				image
+					display block
+					width 138upx
+					height 40upx
+					margin-right 20upx
+				.search
+					display flex
+					justify-content center
+					width 442upx
+					height 56upx
+					line-height 56upx
+					align-items center
+					font-size 28upx
+					border-radius 12.5upx
+					background #ededed
+					image
+						width 28upx
+						height 28upx
+						margin-right 10upx
+					.searchText
+						font-size 28upx
+						color #666
+				.loginButton
+					width 74upx
+					height 40upx
+					line-height 40upx
+					text-align center
+					border 1px solid #dd1a21
+					color #DD1A21
+					font-size 24upx
+					margin-left 16upx
+					border-radius 8upx
+			.headBottom
+				width 750upx
+				height 60upx
+				.hide
+					width 750upx
+					height 60upx
+					.scroll-view
+						display flex
+						flex-wrap nowrap
+						white-space nowrap
+						box-sizing border-box
+						width 650upx
+						height 60upx
+						.scroll-item
+							display inline-block
+							box-sizing border-box
+							height 60upx
+							line-height 60upx
+							text-align center
+							font-size 28upx
+							padding 0 20upx
+							margin-right 20upx
+						.scroll-item:first-child
+							margin-left 30upx
+							color #dd1a21
+							border-bottom 6upx solid #DD1A21
+				.show
+					width 750upx
+					height 372upx
+					box-sizing border-box
+					position absolute
+					left 0
+					top 88upx
+					.showTop
+						width 720upx
+						height 60upx
+						line-height 60upx
+						color #333
+						font-size 28upx
+						margin-left 30upx
+					.showBottom
+						display flex
+						flex-wrap wrap
+						width 720upx
+						height 312upx
+						padding 24upx 0
+						box-sizing border-box
+						margin-left 30upx
+						.show-item
+							width 150upx
+							height 56upx
+							line-height 56upx
+							text-align center
+							font-size 24upx
+							color #333
+							background #fafafa
+							margin 0 30upx 40upx 0
+							box-sizing border-box
+							border 1px solid #ccc
+						.show-item:first-child
+							color #DD1A21
+							border 1px solid #DD1A21
+					.mask
+						width 750upx
+						height 874upx
+						background rgba(0,0,0,0.5)
+						z-index 1
+				.arrow
+					width 100upx
+					height 60upx
+					box-sizing border-box
+					position absolute
+					right 0
+					top 88upx
+					z-index 5
+					image
+						display block
+						width 30upx
+						height 30upx
+						margin 18upx 35upx
+</style>
